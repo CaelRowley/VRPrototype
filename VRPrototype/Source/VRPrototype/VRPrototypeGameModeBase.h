d060098c25fa508dd25f48f0c959fc198a44379f
CaelRowley <caelrowley@gmail.com>
2020-05-26 21:32:46 +0200

    feat: base project

---
+++ VRPrototype/Source/VRPrototype/VRPrototypeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VRPrototypeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRPROTOTYPE_API AVRPrototypeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
