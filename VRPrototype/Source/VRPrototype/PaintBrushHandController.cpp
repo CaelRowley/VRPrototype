0b372202f3d668bbc7a6e0528b2494d8250b88d4
caelrowley <caelrowley@gmail.com>
2021-01-25 19:05:52 +0100

    feat: add controls for valve index

--- VRPrototype/Source/VRPrototype/PaintBrushHandController.cpp
+++ VRPrototype/Source/VRPrototype/PaintBrushHandController.cpp
@@ -14,6 +14,7 @@ APaintBrushHandController::APaintBrushHandController()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	MotionController->SetShowDeviceModel(true);
 }
 
 void APaintBrushHandController::PairControllers(AHandControllerBase* Controller)
